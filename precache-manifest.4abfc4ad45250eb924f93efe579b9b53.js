332ec5705844cbad4fd39bd003dad9ca8c3f25a0
Nick Kasten <kastentx@gmail.com>
2018-12-14 15:10:01 -0600

    Updates

---
+++ precache-manifest.4abfc4ad45250eb924f93efe579b9b53.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3442ee4dbc0228965ffb",
+    "url": "/holiday-photo-booth/static/js/runtime~main.3442ee4d.js"
+  },
+  {
+    "revision": "9040f9abdc21d55ee6b9",
+    "url": "/holiday-photo-booth/static/js/main.9040f9ab.chunk.js"
+  },
+  {
+    "revision": "3016f22c4d3f68445c6c",
+    "url": "/holiday-photo-booth/static/js/1.3016f22c.chunk.js"
+  },
+  {
+    "revision": "9040f9abdc21d55ee6b9",
+    "url": "/holiday-photo-booth/static/css/main.a9925b0e.chunk.css"
+  },
+  {
+    "revision": "5221397c52e1e3199109abc3cff88be8",
+    "url": "/holiday-photo-booth/index.html"
+  }
+];
